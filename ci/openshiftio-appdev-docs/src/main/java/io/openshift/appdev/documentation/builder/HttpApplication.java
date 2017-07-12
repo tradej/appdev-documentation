bfbcef0e41c2819ea7836b78f553612f57fc9772
Tomas Radej <tradej@redhat.com>
2017-07-12 17:26:13 +0200

    Added redirects to HttpApplication

--- ci/openshiftio-appdev-docs/src/main/java/io/openshift/appdev/documentation/builder/HttpApplication.java
+++ ci/openshiftio-appdev-docs/src/main/java/io/openshift/appdev/documentation/builder/HttpApplication.java
@@ -23,12 +23,24 @@ import io.vertx.ext.web.handler.RedirectAuthHandler;
 import io.vertx.ext.web.handler.StaticHandler;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
 import java.util.Properties;
 
 public class HttpApplication extends AbstractVerticle {
 
     private static final String propFileLocation = "/application.properties";
+    private static final Map<String, String> guideUrls = loadGuideUrls();    
     private static final Properties props = loadProperties();
+
+    private static HashMap<String, String> loadGuideUrls() {
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("spring-boot-tomcat", props.getProperty("url.link-spring-boot-runtime-guide")); 
+        map.put("vertx", props.getProperty("url.link-vertx-runtime-guide")); 
+        map.put("wf-swarm", props.getProperty("url.link-wf-swarm-runtime-guide")); 
+        return map;
+    }
 
     private static Properties loadProperties() {
         Properties props = new Properties();
@@ -39,7 +51,7 @@ public class HttpApplication extends AbstractVerticle {
         }
         return props;
     }
-
+    
     @Override
     public void start(Future<Void> future) {
         // Create a router object.
@@ -51,6 +63,14 @@ public class HttpApplication extends AbstractVerticle {
         router.route("/").handler(context -> {
             // Redirect to docs
             context.response().putHeader("location", "/docs").setStatusCode(302).end();
+        });
+        router.getWithRegex("/docs/mission-([^/]+)-(wf-swarm|vertx|spring-boot-tomcat).html").handler(context -> {
+            // Redirect old URLs to refactored URLs
+            String mission = context.request().getParam("param0");
+            String runtime = context.request().getParam("param1");
+            String runtimeUrl = guideUrls.get(runtime);
+            String newUrl = String.format("%s-runtime.html#mission-%s-%s", runtimeUrl, mission, runtime);
+            context.response().putHeader("location", newUrl).setStatusCode(302).end();
         });
         router.get("/docs/*").handler(
                 StaticHandler.create().
